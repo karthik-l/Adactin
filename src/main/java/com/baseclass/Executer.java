366416b0e8ba8af147f099524fbb8ead76db88fd
Karthik L <karthik-l@users.noreply.github.com>
2020-12-09 10:18:32 +0530

    Tested Jar creation

---
+++ src/main/java/com/baseclass/Executer.java
@@ -0,0 +1,10 @@
+package com.baseclass;
+
+import org.junit.runner.JUnitCore;
+
+public class Executer {
+
+	public static void main(String[] args) {
+		JUnitCore.main("com.adactin.runner.Runner");
+	}
+}
